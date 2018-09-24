e78f85850c6ce4bb125118679ea851f14820165e
Gabriel Ron <gaboarp2020@gmail.com>
2018-09-24 19:38:27 -0400

    start project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Hello from './Hello.js';
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
+registerServiceWorker();
