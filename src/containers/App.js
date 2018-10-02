14fa403e4f1470017f86a9583ed1cb575862484d
Gabriel Ron <gaboarp2020@gmail.com>
2018-10-02 19:31:28 -0400

    -Fix App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -25,10 +25,11 @@ class App extends Component {
     }
 
     render() {
-        const filterRobots = this.state.robots.filter(robots =>{
-            return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        const { robots, searchfield } = this.state;
+        const filterRobots = robots.filter(robot =>{
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-        if (this.state.robots.length === 0) {
+        if (!robots.length) {
             return <h1>Loading</h1>
         } else {
             return (
